b21020c4a9830f6cd2bdfdd6b7a4fac44a71dd97
abhishek <abhishekmore710@gmail.com>
2020-12-03 17:15:05 +0530

    timer value changed

--- src/components/timer/timer.js
+++ src/components/timer/timer.js
@@ -4,7 +4,7 @@ import { auth } from '../../firebase/firebase.utils';
 
 
 export default function Timer({totalTime}) {
-  const tmpVal=reactLocalStorage.get('timer', 5400);
+  const tmpVal=reactLocalStorage.get('timer', 5700);
   const [timer, setTimer] = React.useState(tmpVal);
   const id = React.useRef(null);
   const clear = () => {
