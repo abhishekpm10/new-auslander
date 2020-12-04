b09c681cc3bb2b1ee59ae38fa1b58000a5916156
abhishek <abhishekmore710@gmail.com>
2020-12-04 20:04:00 +0530

    favico changed and sign up content changed a little bit

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -40,7 +40,7 @@ class SignIn extends React.Component {
   render() {
     return (
       <div className='sign-in'>
-        <h2>I already have an account</h2>
+        <h2>All the best, enjoy the story</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
